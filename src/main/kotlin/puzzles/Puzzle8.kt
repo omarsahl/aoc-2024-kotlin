bbc22dd2c6c0e7cbc0444de39d41e5c6a2e3680d
Omar Sahl <iamomarsahl@gmail.com>
2024-12-08 23:41:43 +0200

    Puzzle 8.

---
+++ src/main/kotlin/puzzles/Puzzle8.kt
@@ -0,0 +1,107 @@
+package com.omarsahl.puzzles
+
+import com.omarsahl.readFileInput
+
+object Puzzle8 {
+
+    // 276
+    fun part1() {
+        val input = readFileInput("input8.txt")
+        CityMap(input).findAllAntinodes().also(::println)
+    }
+
+    // 991
+    fun part2() {
+        val input = readFileInput("input8.txt")
+        CityMap(input).findAllAntinodesWithResonantHarmonics().also(::println)
+    }
+
+    private class CityMap(val input: List<String>) {
+
+        private val bounds: IntRange
+        private val grid: Array<CharArray>
+        private val antennasMap: Map<Frequency, List<Location>>
+
+        init {
+            val antennas = mutableMapOf<Frequency, MutableList<Location>>()
+            this.grid = Array(input.size) { y ->
+                val row = input[y].toCharArray()
+                row.forEachIndexed { x, frequency ->
+                    if (frequency != EMPTY) {
+                        val antenna = Location(x, y)
+                        antennas.getOrPut(frequency) { mutableListOf() }.add(antenna)
+                    }
+                }
+                row
+            }
+            this.bounds = grid.indices
+            this.antennasMap = antennas
+        }
+
+        fun findAllAntinodes(): Int {
+            val antinodes = hashSetOf<Location>()
+            forEachAntennasPair { antenna1, antenna2 ->
+                antenna1.antinodeOppositeTo(antenna2).also {
+                    if (it.withinBounds()) antinodes.add(it)
+                }
+
+                antenna2.antinodeOppositeTo(antenna1).also {
+                    if (it.withinBounds()) antinodes.add(it)
+                }
+            }
+            return antinodes.size
+        }
+
+        fun findAllAntinodesWithResonantHarmonics(): Int {
+            val antinodes = hashSetOf<Location>()
+            forEachAntennasPair { antenna1, antenna2 ->
+                var dx = antenna2.x - antenna1.x
+                var dy = antenna2.y - antenna1.y
+                var antinode = antenna1
+                while (antinode.withinBounds()) {
+                    antinodes.add(antinode)
+                    antinode = antinode.plus(dx, dy)
+                }
+
+                dx *= -1
+                dy *= -1
+                antinode = antenna2
+                while (antinode.withinBounds()) {
+                    antinodes.add(antinode)
+                    antinode = antinode.plus(dx, dy)
+                }
+            }
+
+            return antinodes.size
+        }
+
+        private fun Location.withinBounds(): Boolean = x in bounds && y in bounds
+
+        private inline fun forEachAntennasPair(action: (antenna1: Location, antenna2: Location) -> Unit) {
+            for ((_, antennas) in antennasMap) {
+                if (antennas.size < 2) continue
+
+                for (i in antennas.indices) {
+                    for (j in i + 1..antennas.lastIndex) {
+                        action(antennas[i], antennas[j])
+                    }
+                }
+            }
+        }
+    }
+
+    private data class Location(val x: Int, val y: Int) {
+
+        fun plus(dx: Int, dy: Int): Location = copy(x = x + dx, y = y + dy)
+
+        fun antinodeOppositeTo(other: Location): Location {
+            val dx = x - other.x
+            val dy = y - other.y
+            return Location(x + dx, y + dy)
+        }
+    }
+
+    private const val EMPTY = '.'
+}
+
+private typealias Frequency = Char
